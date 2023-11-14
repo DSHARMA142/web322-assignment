dc761f48b82bec18cf3551613fcaf84cd2229b57
DSHARMA142 <dsharma142@myseneca.ca>
2023-11-13 21:43:06 -0500

    draft

--- server.js
+++ server.js
@@ -6,7 +6,7 @@
 * 
 *  https://www.senecacollege.ca/about/policies/academic-integrity-policy.html
 * 
-*  Name: Divyanshu Sharma Student ID: 172551210 Date: 30 October 2023
+*  Name: Divyanshu Sharma Student ID: 172551210 Date: 12 November 2023
 *
 *  Published URL: https://frantic-ox-pants.cyclic.app
 *
@@ -15,59 +15,61 @@
 
 
 const legoData = require("./modules/legoSets");
-const express = require("express");
+const express = require('express');
 const path = require("path");
-const app = express();
-const HTTP_PORT = process.env.PORT || 8080;
-app.use(express.static("public"));
-legoData.initialize();
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/views/home.html"));
-});app.get("/about", function (req, res) {
-  res.sendFile(path.join(__dirname, "/views/about.html"));
-});app.get("/404", function (req, res) {
-  res.sendFile(path.join(__dirname, "/views/404.html"));
+const app=express();
+const HTTP_PORT = process.env.PORT || 8080; 
+app.set('view engine', 'ejs');
+app.use(express.static('public'));
+legoData.initialize().then(()=>{
+  app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
+})
+app.get("/",(req, res)=> {
+  res.render("home");
 });
-
-app.get("/lego/sets", async (req, res) => {
-  const theme = req.query.theme;
-
-  try {
-    let data;
-
-    if (theme) {
-      data = await legoData.getSetsByTheme(theme);
-    } else {
-      data = await legoData.getAllSets();
-    }
-
-    if (data.length === 0) {
-      res.sendFile(path.join(__dirname, "/views/404.html"));
-    } else {
-      res.json(data);
-    }
-  } catch (error) {
-    res.sendFile(path.join(__dirname, "/views/404.html"));
-  }
+app.get("/about", function (req, res) {
+  res.render("about");
 });
-app.get("/lego/sets/:setNum", async (req, res) => {
-  const setNum = req.params.setNum;
-
-  try {
-    const data = await legoData.getSetByNum(setNum);
-
-    if (data) {
-      res.json(data);
-    } else {
-      res.sendFile(path.join(__dirname, "/views/404.html"));
-    }
-  } catch (error) {
-    res.sendFile(path.join(__dirname, "/views/404.html"));
-  }
+app.get("/404", function (req, res) {
+  res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for"});
 });
-
-app.listen(HTTP_PORT, () => console.log(`server listening on: ${HTTP_PORT}`));
-
-
-
+  app.get('/lego/sets', (req, res) => {
+    const theme=req.query.theme;
+    if(theme){
+      legoData.getSetsByTheme(theme).then((data)=>{
+        if(data.length===0){
+          res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for"});
+        }else{
+          res.render("sets",{
+            data: data
+          })
+        }
+    }).catch((error)=>{
+      res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for"});
+    })
+    }else{
+      legoData.getAllSets().then((data)=>{
+        res.render("sets",{
+          data: data
+        })
+      }).catch((error)=>{
+        res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for"});
+      })   
+    }
+  });
+  app.get('/lego/sets/:setNum', (req, res) => {
+    const setNum = req.params.setNum;
+    legoData.getSetByNum(setNum)
+        .then((data) => {
+            if (data) {
+                res.render("set",{
+                  set: data
+                });
+            } else {
+              res.render("404")
+            }
+        })
+        .catch((error) => {
+          res.status(404).render("404", {message: "I'm sorry, we're unable to find what you're looking for"});
+        });
+});
